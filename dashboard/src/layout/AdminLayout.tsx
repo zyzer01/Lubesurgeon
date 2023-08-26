2a6029a38a167deee7c8b4af6fb30a4a609dd83c
zyzer01 <davidicfola@gmail.com>
2023-08-26 09:33:10 +0000

    Separated admin header

--- dashboard/src/layout/AdminLayout.tsx
+++ dashboard/src/layout/AdminLayout.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import Header from '../components/Header';
+import AdminHeader from '../components/AdminHeader';
 import SidebarAdmin from '../components/SidebarAdmin';
 import { Outlet } from 'react-router-dom';
 
@@ -17,7 +17,7 @@ const AdminLayout = () => {
         {/* <!-- ===== Content Area Start ===== --> */}
         <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
           {/* <!-- ===== Header Start ===== --> */}
-          <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+          <AdminHeader sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
           {/* <!-- ===== Header End ===== --> */}
 
           {/* <!-- ===== Main Content Start ===== --> */}
