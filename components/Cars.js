d3550b9835d96519874e7aa3770ef6ec92d1bfe3
Onifade David <davidicfola@gmail.com>
2023-08-31 20:10:55 +0100

    Swiper js

--- components/Cars.js
+++ components/Cars.js
@@ -1,14 +1,53 @@
-import React from 'react';
+'use client';
+
+import React, { useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Grid, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/autoplay';
+import 'swiper/css/grid';
 import Heading from './utilities/Heading';
-import Flexitems from './utilities/Flexitems';
+
+// SwiperCore.use([Grid, Autoplay]);
 
 function Cars(props) {
+  const carBrands = [
+    '/images/cars/acura-logo-vector-01 1.png',
+    '/images/cars/aston-martin-logo-vector-01 1.png',
+    '/images/cars/aston-martin-logo-vector-01 1.png',
+    '/images/cars/aston-martin-logo-vector-01 1.png',
+    '/images/cars/aston-martin-logo-vector-01 1.png',
+    '/images/cars/aston-martin-logo-vector-01 1.png',
+    '/images/cars/audi-logo-vector-download 1.png',
+    '/images/cars/audi-logo-vector-download 1.png',
+    '/images/cars/audi-logo-vector-download 1.png',
+    '/images/cars/audi-logo-vector-download 1.png',
+    '/images/cars/audi-logo-vector-download 1.png',
+    '/images/cars/audi-logo-vector-download 1.png',
+  ];
   return (
-    <div>
-      <section>
-        <Heading headTitle="Cars" sub="We service nearly all makes and models." />
-      </section>
-    </div>
+    <>
+      <Swiper
+        modules={[Grid, Autoplay]}
+        spaceBetween={20}
+        slidesPerView={4}
+        grid={{
+          rows: 4,
+          fill: 'row',
+        }}
+        autoplay={{
+          delay: 1500,
+          disableOnInteraction: false,
+        }}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}>
+        {carBrands.map((brand, index) => (
+          <SwiperSlide key={index}>
+            <img src={brand} alt={`Car Brand ${index}`} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
   );
 }
 
