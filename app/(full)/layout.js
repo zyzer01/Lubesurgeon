3ad431089ee5fa348e4fd7166a4f577227ead668
Onifade David <davidicfola@gmail.com>
2023-09-03 16:38:25 +0100

    Contact page on dashboard

--- app/(full)/layout.js
+++ app/(full)/layout.js
@@ -1,10 +1,7 @@
 'use client';
 import '../globals.css';
-import { Inter } from 'next/font/google';
 import Nav from '@/components/Nav';
 import Footer from '@/components/Footer';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function FullLayout({ children }) {
   return (
