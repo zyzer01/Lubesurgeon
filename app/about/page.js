245d60eaea234135c47f6914acc04b1d117e57b6
zyzer01 <davidicfola@gmail.com>
2023-09-05 17:38:31 +0000

    Metadata

--- app/about/page.js
+++ app/about/page.js
@@ -4,29 +4,30 @@ import { OtherHero } from "@/components/OtherHero";
 import { OtherFeatures } from "@/components/OtherFeatures";
 import { ContentOne } from "@/components/ContentOne";
 
-
 const featuredItems = [
   {
     title: "Total Transparency",
     content:
       "We provide clear and honest pricing, ensuring you always know what to expect.",
-    button: 'Learn more',
-    },
+    button: "Learn more",
+  },
   {
     title: "Swiftness to the Job",
-    content: "Our team is known for swift and efficient service to get you back on the road as soon as possible.",
-    button: 'Learn more',
+    content:
+      "Our team is known for swift and efficient service to get you back on the road as soon as possible.",
+    button: "Learn more",
   },
   {
     title: "Reliability",
-    content: "Count on us for consistent and dependable vehicle maintenance solutions.",
-    button: 'Learn more',
+    content:
+      "Count on us for consistent and dependable vehicle maintenance solutions.",
+    button: "Learn more",
   },
 ];
 
 const About = () => {
   return (
-    <>
+    <div>
       <OtherHero
         heading="Welcome to Lubesurgeons"
         sub="Your Trusted Partner for Vehicle Maintenance and Care"
@@ -35,7 +36,7 @@ const About = () => {
       />
       <OtherFeatures items={featuredItems} />
       <ContentOne />
-    </>
+    </div>
   );
 };
 
--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,35 @@ import Nav from '@/components/Nav';
 import Footer from '@/components/Footer';
 import { jakarta } from './font'
 
+export const metadata = {
+  title: 'Lubesurgeons - Your Mobile Mechanic Patner',
+  description: "At Lubesurgeons, we're on a mission to transform the way you experience vehicle maintenance. Say goodbye to the hassle of traditional garages and hello to the convenience of our certified mobile mechanics.",
+  openGraph: {
+    title: 'Lubesurgeons - Your Mobile Mechanic Patner',
+    description: "At Lubesurgeons, we're on a mission to transform the way you experience vehicle maintenance. Say goodbye to the hassle of traditional garages and hello to the convenience of our certified mobile mechanics.",
+    image: {
+      url: 'https://yourwebsite.com/images/og-image-homepage.jpg',
+      width: 1200,
+      height: 630,
+    },
+  },
+  twitter: {
+    cardType: 'summary_large_image',
+    image: {
+      url: 'https://yourwebsite.com/images/twitter-image-homepage.jpg',
+      width: 1200,
+      height: 600,
+    },
+  },
+  whatsapp: {
+    image: {
+      url: 'https://yourwebsite.com/images/whatsapp-image-homepage.jpg',
+      width: 800,
+      height: 400,
+    },
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={jakarta.className}>
