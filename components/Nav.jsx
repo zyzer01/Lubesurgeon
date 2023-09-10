1360cc2d930eaa1d260d1ed06de325e3052940da
Onifade David <davidicfola@gmail.com>
2023-09-10 23:18:54 +0100

    Meta graph

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -41,7 +41,7 @@ function Nav() {
           <div className="hidden md:flex justify-between items-center">
             <div className="">
               <Link href="/">
-                <Image src={logo} width={200} height={350} alt="Workflow" />
+                <Image src={logo} width={250} height={400} alt="Workflow" />
               </Link>
             </div>
             <div className="">
@@ -67,14 +67,14 @@ function Nav() {
         <div className="flex justify-between px-4 pb-4">
           <div className="flex md:hidden">
             <Link href="/">
-              <Image src={logo} alt="Workflow" />
+              <Image src={logo} width={250} height={400} alt="Workflow" />
             </Link>
           </div>
           <div className="-mr-2 flex md:hidden">
             <button
               onClick={toggleMenu}
               type="button"
-              className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-200 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
               aria-controls="mobile-menu"
               aria-expanded={isOpen ? 'true' : 'false'}>
               <span className="sr-only">Open main menu</span>
