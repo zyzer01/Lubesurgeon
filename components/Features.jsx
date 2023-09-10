cde869d66d25031e6cf8b900716799f9252ff3df
Onifade David <davidicfola@gmail.com>
2023-09-10 23:24:00 +0100

    Meta graph

--- components/Features.jsx
+++ components/Features.jsx
@@ -1,34 +1,33 @@
-import Heading from "./utilities/Heading";
-import Sub from "./utilities/Sub";
-import Image from "next/image";
-import schedule from "/public/images/shedule feature.png";
-import quote from "/public/images/get a quote.png";
-import maintenance from "/public/images/car maintenance.png";
-import wallet from "/public/images/wallet pay.png";
+import Heading from './utilities/Heading';
+import Sub from './utilities/Sub';
+import Image from 'next/image';
+import schedule from '/public/images/shedule feature.png';
+import quote from '/public/images/get a quote.png';
+import maintenance from '/public/images/car maintenance.png';
+import wallet from '/public/images/wallet pay.png';
 
 const items = [
   {
     src: schedule,
     subText: (
       <span>
-        Schedule your LubeSurgery using the{' '}
-        <a href='https://www.lubesurgeons.com/dashboard'>Website</a>
+        Schedule your LubeSurgery using the <a href="https://dashboard.lubesurgeons.com">Website</a>
       </span>
-    )
+    ),
   },
   {
     src: quote,
-    subText:  " Get a Quote. Provide your location and our mechanics come to you."
+    subText: ' Get a Quote. Provide your location and our mechanics come to you.',
   },
   {
     src: maintenance,
-    subText:  "Track your services, text your mechanic, receive notifications."
+    subText: 'Track your services, text your mechanic, receive notifications.',
   },
   {
     src: wallet,
-    subText:  "Rate and pay when your service is complete."
-  }
-]
+    subText: 'Rate and pay when your service is complete.',
+  },
+];
 
 function Features(props) {
   return (
@@ -39,11 +38,11 @@ function Features(props) {
           <div className="grid grid-cols-2 gap-4 md:gap-0 md:grid-cols-4 px-8 justify-center items-center text-center">
             {items.map((item, index) => (
               <div key={index}>
-              <div className="flex justify-center">
-                <Image src={item.src} width={130} height={60} alt="schedule" />
+                <div className="flex justify-center">
+                  <Image src={item.src} width={130} height={60} alt="schedule" />
+                </div>
+                <Sub subText={item.subText} />
               </div>
-              <Sub subText={item.subText}/>
-            </div>
             ))}
           </div>
         </div>
