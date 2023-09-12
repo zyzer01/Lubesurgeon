9a7515bebee91adfe27c5ba050ba636c506f1f74
Onifade David <davidicfola@gmail.com>
2023-09-12 16:34:57 +0100

    Add: Cars section

--- app/page.js
+++ app/page.js
@@ -80,7 +80,7 @@ const Home = () => {
       <Features />
       <ServiceSection />
       <Choose />
-      <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div class="px-4 pt-16 pb-8 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
         <Heading headTitle="Frequently Asked Questions" />
         <div class="max-w-xl sm:mx-auto lg:max-w-2xl">
           <div class="space-y-4">
